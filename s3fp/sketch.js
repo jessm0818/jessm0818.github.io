0ef1af616aac0d61ed8fe3b0ce12ee2e22006e3c
jessm0818 <jessm818@sas.upenn.edu>
2024-11-25 11:45:29 -0500

    Add files via upload

---
+++ s3fp/sketch.js
@@ -0,0 +1,37 @@
+let colors = [];
+let circleSize = 50;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  noStroke();
+  
+  colors = Array(10).fill().map(() => 
+  color(random(0, 255), random(0, 255), random(0, 255)));
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseMoved() {
+  Ink();
+}
+
+function Ink() {
+  for (let i = 0; i < random(1, 5); i++) {
+    fill(random(colors));
+    let size = random(10, circleSize);
+    let x = mouseX + random(-50, 50);
+    let y = mouseY + random(-50, 50);
+    ellipse(x, y, size);
+  }
+}
+
+function keyPressed() {
+  if (key === 'ArrowUp') {
+    circleSize += 10;
+  } else if (key === 'ArrowDown') {
+    circleSize = max(10, circleSize - 10);
+  }
+}
